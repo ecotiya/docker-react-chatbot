45fc2545d926aef8732d52c64bd54e42ef0730e5
ecotiya <ghqyydf829@gmail.com>
2021-07-07 20:19:26 +0900

    update components

---
+++ react-app/src/components/Answer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+// import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+// const useStyles = makeStyles((theme: Theme) =>
+//   createStyles({
+//     root: {
+//
+//     },
+//   }),
+// );
+
+const Answer = (props) => {
+  // const classes = useStyles();
+  return (
+    <Button variant="contained" color="primary" onClick = {() => props.select(props.content, props.nextId)}>
+      {props.content}
+    </Button>
+  )
+}
+
+export default Answer
---
+++ react-app/src/components/Chat.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Onushi from '../assets/img/onushi.jpg'
+import Ecotiya from '../assets/img/ecotiya.jpg'
+
+const Chat = (props) => {
+  const isQuestion = (props.type === 'question');
+  const classes = isQuestion ? 'p-chat__row' : 'p-chat__reverse';
+
+  return (
+    <ListItem className = {classes}>
+      <ListItemAvatar>
+        {isQuestion ? (
+          <Avatar alt="icon" src={Ecotiya} />
+        ) : (
+          <Avatar alt="icon" src={Onushi} />
+        )}
+      </ListItemAvatar>
+      <div className = "p-chat__bubble">{props.text}</div>
+    </ListItem>
+  );
+};
+
+export default Chat
