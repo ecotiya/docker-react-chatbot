7952301796baf980966811c9a118d41a342cd24e
ecotiya <ghqyydf829@gmail.com>
2021-07-08 08:56:00 +0900

    update chatbot scrollArea

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -33,10 +33,20 @@ export default class App extends React.Component {
   selectAnswer = (selectedAnswer, nextQuestionId) => {
     switch (true) {
       case (nextQuestionId === 'init'):
-        this.displayNextQuestion(nextQuestionId);
+        setTimeout(() => this.displayNextQuestion(nextQuestionId), 500);
         break;
 
+      case /^https:*/.test(nextQuestionId):
+        {
+        const a = document.createElement('a');
+        a.href = nextQuestionId;
+        a.target = '_blank';
+        a.click();
+        break;
+        }
+
       default:
+        {
         const chats = this.state.chats;
         chats.push({
             text : selectedAnswer,
@@ -47,14 +57,22 @@ export default class App extends React.Component {
            chats: chats
         })
 
-        this.displayNextQuestion(nextQuestionId);
+        setTimeout(() => this.displayNextQuestion(nextQuestionId), 500);
         break;
+        }
     }
   }
 
   componentDidMount() {
     const initAnswer = "";
     this.selectAnswer(initAnswer, this.state.currentId);
+  }
+
+  componentDidUpdate() {
+    const scrollArea = document.getElementById('scroll-area')
+    if (scrollArea) {
+      scrollArea.scrollTop = scrollArea.scrollHeight;
+    }
   }
 
   render() {
